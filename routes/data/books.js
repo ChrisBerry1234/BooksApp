ef9fdb80ad1eb3046fbcfd0d3062d12998b954f7
ChrisBerry1234 <christopherjberry846@gmail.com>
2026-02-03 22:59:19 -0500

    Create books.js to hold book information with 0(1) lookup time

---
+++ routes/data/books.js
@@ -0,0 +1,13 @@
+const booksById = {
+  1: { id: 1, title: "The Great Gatsby", author: "F. Scott Fitzgerald", reviews: [] },
+  2: { id: 2, title: "Things Fall Apart", author: "Chinua Achebe", reviews: [] },
+  3: { id: 3, title: "Fairy Tales", author: "Hans Christian Andersen", reviews: [] },
+  4: { id: 4, title: "The Divine Comedy", author: "Dante Alighieri", reviews: [] },
+  5: { id: 5, title: "The Epic Of Gilgamesh", author: "Unknown", reviews: [] },
+  6: { id: 6, title: "The Book Of Job", author: "Unknown", reviews: [] },
+  7: { id: 7, title: "One Thousand and One Nights", author: "Unknown", reviews: [] },
+  8: { id: 8, title: "Njál's Saga", author: "Unknown", reviews: [] },
+  9: { id: 9, title: "Pride and Prejudice", author: "Jane Austen", reviews: [] },
+  10: { id: 10, title: "Le Père Goriot", author: "Honoré de Balzac", reviews: [] },
+  11: { id: 11, title: "Molloy, Malone Dies, The Unnamable", author: "Samuel Beckett", reviews: [] }
+};
